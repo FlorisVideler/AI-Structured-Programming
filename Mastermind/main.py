66427d7e8e9c16265c0e76dee92b78ed4e46216c
Floris Videler <60732920+FlorisVideler@users.noreply.github.com>
2020-02-18 10:25:25 +0100

    Update main.py
    Ziet er best goed uit. Sommige dingen heb je heel slim gedaan en op een manier die je niet vaak ziet.
    Naar mijn inzicht best weinig op aan te merken.

--- Mastermind/main.py
+++ Mastermind/main.py
@@ -1,12 +1,22 @@
 import colors
-import algorithms as algo
+import algo as algo
 import time
+
+""""
+Wat algemene dingen die je wss zelf al wou doen:
+1. Always try to explain yourself in code.
+2. Don't comment out code. Just remove.
+Uit https://gist.github.com/wojteklu/73c6914cc446146b8b533c0988cf8d29
+
+"""
 
 codeLength = 4
 maxTries = 8
 amountOfColors = len(colors.all_colors)
-userSecret = [] # list of x long (usually 4) with names of colours
+userSecret = []  # list of x long (usually 4) with names of colours
 
+
+"""Slimme manier om de game mode aan te roepen!"""
 def checkInputOptions(_input: str, options: dict, callback) -> str:
     if _input in options:
         options[_input]()
@@ -15,102 +25,123 @@ def checkInputOptions(_input: str, options: dict, callback) -> str:
         print(f"I cant't understand this input: {_input}")
         callback()
 
+
 def selectGameMode():
-   mode = input('Please select the gamemode you want to play.\n1: AI vs AI, see the result of a guessing algorithm.\n2: You vs Ai, You input all the guesses and and asses the responses.\n')
-   # \n3: AI vs You, You input a secret and respond to the guesses being made by the AI.
-   checkInputOptions(mode,{'1':AIvsAI,'AI vs AI':AIvsAI,'2':personVsAi,'You vs AI':personVsAi},selectGameMode)
+    mode = input(
+        'Please select the gamemode you want to play.\n1: AI vs AI, see the result of a guessing algorithm.\n2: You vs Ai, You input all the guesses and and asses the responses.\n')
+    # \n3: AI vs You, You input a secret and respond to the guesses being made by the AI.
+    checkInputOptions(mode, {'1': AIvsAI, 'AI vs AI': AIvsAI, '2': personVsAi, 'You vs AI': personVsAi}, selectGameMode)
+
+
 def selectGameOptions():
-   checkInputOptions(input("Do want to continue with normal game options?\n8 max tries?\n6 different colors?(wip)\nCode length of 4?(wip)\ny/n?"),{'n':gameOptions,'no':gameOptions},selectGameOptions)
+    checkInputOptions(input(
+        "Do want to continue with normal game options?\n8 max tries?\n6 different colors?(wip)\nCode length of 4?(wip)\ny/n?"),
+                      {'n': gameOptions, 'no': gameOptions}, selectGameOptions)
+
 
 def gameOptions():
-   checkInputOptions(input("Which option do you want to change?\n1: Max tries?\n2: Amount of different colors?(wip)\n3: Code length(wip)?\n"),{'1':setOptionAmount},gameOptions)
+    checkInputOptions(input(
+        "Which option do you want to change?\n1: Max tries?\n2: Amount of different colors?(wip)\n3: Code length(wip)?\n"),
+                      {'1': setOptionAmount}, gameOptions)
+
 
 def setOptionAmount():
-   print('test')
+    print('test')
+
 
 def AIvsAI():
-   wins = 0
-   loses = 0
-   amountOfGames = input("Howmany games do you want to play?\n")
-   game = 0
-   print(f'Secret: {colors.SECRET}')
-   while game < int(amountOfGames):
-      print(f'New Round, round {game}')
-      for turn in range(0,maxTries):
-         print(f'Turn:{turn+1}')
-         guesse = inputToGuesse()
-         evaluation = colors.evaluateColors(guesse,colors.SECRET)
-         print(evaluation)
-         if(evaluation['black'] == 4):
-            print('-----\nYou won\n-----')
-            hasWon = True
-            break
-      game += 1
+    wins = 0
+    loses = 0
+    """Een beetje overbodige feedback, maar je gebruikt op andere plekken wel exceptions (zoals ValueError). Misschien een simpele toevoeging om dat dan op andere plekken waar dit kan ook te doen"""
+    amountOfGames = input("Howmany games do you want to play?\n")
+    game = 0
+    print(f'Secret: {colors.SECRET}')
+    """Kan een ValueError veroorzaken"""
+    while game < int(amountOfGames):
+        print(f'New Round, round {game}')
+        for turn in range(0, maxTries):
+            print(f'Turn:{turn + 1}')
+            guesse = inputToGuesse()
+            evaluation = colors.evaluateColors(guesse, colors.SECRET)
+            print(evaluation)
+            if (evaluation['black'] == 4):
+                print('-----\nYou won\n-----')
+                hasWon = True
+                break
+        game += 1
 
-      if(hasWon):
-         wins += 1
-      else:
-         loses += 1
-      
+        """Haakjes zijn niet nodig"""
+        if (hasWon):
+            wins += 1
+        else:
+            loses += 1
 
-   print(f'Player 1 Won:{wins} times and Lost:{loses} times')
+    print(f'Player 1 Won:{wins} times and Lost:{loses} times')
+
 
 def personVsAi():
-   wins = 0
-   loses = 0
-   amountOfGames = input("Howmany games do you want to play?\n")
-   game = 0
-   print(f'Secret: {colors.SECRET}')
-   while game < int(amountOfGames):
-      print(f'New Round, round {game}')
-      for turn in range(0,maxTries):
-         print(f'Turn:{turn+1}')
-         guesse = inputToGuesse()
-         evaluation = colors.evaluateColors(guesse,colors.SECRET)
-         print(evaluation)
-         if(evaluation['black'] == 4):
-            print('-----\nYou won\n-----')
-            hasWon = True
-            break
-      game += 1
+    wins = 0
+    loses = 0
+    amountOfGames = input("Howmany games do you want to play?\n")
+    game = 0
+    print(f'Secret: {colors.SECRET}')
+    """Kan een ValueError veroorzaken"""
+    while game < int(amountOfGames):
+        print(f'New Round, round {game}')
+        for turn in range(0, maxTries):
+            print(f'Turn:{turn + 1}')
+            guesse = inputToGuesse()
+            evaluation = colors.evaluateColors(guesse, colors.SECRET)
+            print(evaluation)
+            """Haakjes zijn niet nodig"""
+            if (evaluation['black'] == 4):
+                print('-----\nYou won\n-----')
+                hasWon = True
+                break
+        game += 1
 
-      if(hasWon):
-         wins += 1
-      else:
-         loses += 1
-      
+        """Haakjes zijn niet nodig"""
+        if (hasWon):
+            wins += 1
+        else:
+            loses += 1
 
-   print(f'You Won:{wins} times and Lost:{loses} times')
+    print(f'You Won:{wins} times and Lost:{loses} times')
 
-def inputToGuesse()->list:
-   newGuesse = []
-   print(f'\nPlease input {codeLength} colors:')
-   print("1: Red, 2: Yellow, 3: Blue, 4: Green, 5: Orange, 6: Purple")
-   while len(newGuesse) < 4:
-      color = inputToColor(input('Please input one of these colors.\n'))
-      if color:
-         newGuesse.append(color)
-   return newGuesse
 
-def inputToColor(i)->str:
-   try:
-      i = int(i)
-      if(i <= amountOfColors):
-         print('1',colors.all_colors[i-1])
-         return colors.all_colors[i-1]
-      else:
-         print('number was to high')
-   except ValueError:
-      for color in colors.all_colors:
-         if lower(i[0]) == lower(color[0]):
-            print('1',color)
-            return color
+def inputToGuesse() -> list:
+    newGuesse = []
+    print(f'\nPlease input {codeLength} colors:')
+    print("1: Red, 2: Yellow, 3: Blue, 4: Green, 5: Orange, 6: Purple")
+    while len(newGuesse) < 4:
+        color = inputToColor(input('Please input one of these colors.\n'))
+        if color:
+            newGuesse.append(color)
+    return newGuesse
+
+
+def inputToColor(i) -> str:
+    try:
+        i = int(i)
+        if (i <= amountOfColors):
+            print('1', colors.all_colors[i - 1])
+            return colors.all_colors[i - 1]
+        else:
+            print('number was to high')
+
+    except ValueError:
+        for color in colors.all_colors:
+            """"Waar komt de lower functie vandaan?"""
+            if lower(i[0]) == lower(color[0]):
+                print('1', color)
+                return color
+
 
 if __name__ == "__main__":
-   # selectGameOptions()
-   selectGameMode()
-   # inputToGuesse()
-   # pins = colors.evaluateColors(['Green','Green','Red','Yellow'],colors.SECRET)
-   # print(pins)
-   # print(len(algo.allPossibleCombinations))
-   # print('done')
+    # selectGameOptions()
+    selectGameMode()
+    # inputToGuesse()
+    # pins = colors.evaluateColors(['Green','Green','Red','Yellow'],colors.SECRET)
+    # print(pins)
+    # print(len(algo.allPossibleCombinations))
+    # print('done')
